275e956e9709dd8ad0dce242b6d1d1ba24d7516c
Abhimanyu Singh <superhit0@gmail.com>
2020-03-21 14:46:10 +0530

    Added app with css

--- src/models/App.js
+++ src/models/App.js
@@ -2,13 +2,27 @@ function List() {}
 
 function App() {
   this.lists = [];
+  this.container = null;
 }
+
+const getAddHtmlElement = () => {
+  const element = document.createElement('div');
+  element.classList.add('add-list-container');
+  const iTag = document.createElement('i');
+  iTag.className= "fas fa-plus-circle";
+  element.appendChild(iTag);
+  return element;
+};
 
 App.prototype.removeList = (index) => this.lists.splice(index, 1);
 App.prototype.addList = (listName) => this.lists.push(new List(listName));
+App.prototype.setContainer = (element) => this.container = element;
 App.prototype.getHtmlElement = () => {
   const element = document.createElement('div');
   element.classList.add('app-container');
+  // this.lists.forEach(list => element.appendChild(list.getHtmlElement()));
+  const addHtmlElement = getAddHtmlElement();
+  element.appendChild(addHtmlElement);
   return element;
 };
 
